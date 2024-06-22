b93aab46b60f5cbbad24506e796c33c4fbb7e679
Filippo Orru <filippoorru@hotmail.com>
2024-06-22 18:35:54 +0200

    Fix infinite water

--- WaterBackground.gd
+++ WaterBackground.gd
@@ -4,6 +4,8 @@ extends TileMap
 @onready var activeCamera: Camera2D = get_viewport().get_camera_2d()
 
 func _process(delta):
-	self.position.x = int(activeCamera.position.x) / GameWorld.GRID_SIZE
-	self.position.y = int(activeCamera.position.y) / GameWorld.GRID_SIZE
-	print(self.position.x, self.position.y)
+	var x = int(activeCamera.global_position.x) / GameWorld.GRID_SIZE / 2
+	var y = int(activeCamera.global_position.y) / GameWorld.GRID_SIZE / 2
+	
+	self.position.x = x * GameWorld.GRID_SIZE * 2
+	self.position.y = y * GameWorld.GRID_SIZE * 2
