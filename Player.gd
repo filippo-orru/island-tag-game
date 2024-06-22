65de88fde5b076229149fa3593cefd79e078629f
Valentin Slawicek <valentin.slawi@gmail.com>
2024-06-22 14:32:59 +0200

    Grid based movement

--- Player.gd
+++ Player.gd
@@ -1,20 +1,40 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const GRID_SIZE = 32
 
-func _physics_process(delta): # Don't use delta here, move_and_slide already respects it.
-	# Get the input direction and handle the movement/deceleration.
+var fromPos = [0, 0]
+var targetPos = [0, 0]
+
+func nextMove():
+	fromPos[0] = targetPos[0]
+	fromPos[1] = targetPos[1]
 	
+	# TODO save input "last timee direction was pressed" to make not left/right take precedence
 	var xDirection = Input.get_axis("ui_left", "ui_right")
-	if xDirection:
-		velocity.x = xDirection * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED / 10) # Slow down
-		
 	var yDirection = Input.get_axis("ui_up", "ui_down")
-	if yDirection:
-		velocity.y = yDirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED / 10) # Slow down
+	
+	if xDirection:
+		# TODO -1, 0 or 1. no floats on dpad
+		targetPos[0] += xDirection
+		get_node(".").look_at(self.position + Vector2(1 * xDirection, 0))
+	elif yDirection:
+		targetPos[1] += yDirection
+		get_node(".").look_at(self.position + Vector2(0, 1 * yDirection))
 
-	move_and_slide()
+
+func _process(delta):
+	# Move left/right
+	if fromPos[0] != targetPos[0]:
+		position.x = move_toward(position.x, targetPos[0] * GRID_SIZE, delta * SPEED)
+		if position.x == targetPos[0] * GRID_SIZE:
+			nextMove()
+	
+	# Move top/down
+	if fromPos[1] != targetPos[1]:
+		position.y = move_toward(position.y, targetPos[1] * GRID_SIZE, delta * SPEED)
+		if position.y == targetPos[1] * GRID_SIZE:
+			nextMove()
+			
+	if fromPos == targetPos:
+		nextMove()
