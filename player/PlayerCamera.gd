f44aabe3a359fd2fd83105309516c17c2f21e13a
Valentin Slawicek <valentin.slawi@gmail.com>
2024-06-23 14:00:18 +0200

    Add camera shake function

---
+++ player/PlayerCamera.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+
+var shakeStrength: float = 30.0
+var shakeFade: float = 5.0
+
+var _rng = RandomNumberGenerator.new()
+var _currentShakeStrength: float = 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if _currentShakeStrength > 0:
+		_currentShakeStrength = lerpf(_currentShakeStrength, 0, shakeFade * delta)
+		
+	offset = randomOffset()
+
+
+func shake():
+	_currentShakeStrength = shakeStrength
+	
+func randomOffset() -> Vector2:
+	return Vector2(
+		_rng.randf_range(-_currentShakeStrength, _currentShakeStrength),
+		_rng.randf_range(-_currentShakeStrength, _currentShakeStrength)
+		)
