6d06cb100d0dfc5405701ad27cc8fa0f0d730844
Valentin Slawicek <valentin.slawi@gmail.com>
2024-06-23 16:29:29 +0200

    Revert "Try fixing multiplayer. DOESNT WORK"
    This reverts commit 3e14e940e696ac9d55653b652f1cfabbbcc3619a.

--- player/Player.gd
+++ player/Player.gd
@@ -11,21 +11,14 @@ const RUNNER_PATCH_RECT = Rect2(0, 272, 48, 48)
 @export var fromPos = Vector2i(0, 0)
 @export var targetPos = Vector2i(0, 0)
 @export var currentMovementSpeed = 0.0
-@export var hunter := false:
-	set(value):
-		hunter = value
-		if hunter:
-			$Control/MarginContainer/NinePatchRect.region_rect = HUNTER_PATCH_RECT
-		else:
-			$Control/MarginContainer/NinePatchRect.region_rect = RUNNER_PATCH_RECT
-			
+@export var hunter = false
 @export var tagged = false
 @export var score = 0
 
 @export var playerName: String
 
 # Set by the authority, synchronized on spawn.
-@export var playerId := 1:
+@export var playerId := 1 :
 	set(id):
 		playerId = id
 		# Give authority over the player input to the appropriate peer.
@@ -39,6 +32,7 @@ var controller: PlayerControllStrategy
 func _ready():
 	fromPos = Vector2i(position / GRID_SIZE)
 	targetPos = Vector2i(position / GRID_SIZE)
+	set_hunter(hunter)
 
 func _input(event):
 	controller._input(event)
@@ -119,27 +113,23 @@ func _on_area_entered(area):
 	#  [ ] taggedPlayer also gets hunter
 	#  [x] switch hunter is switche
 	print(self.playerName, " tagged ", taggedPlayer.playerName)
+	taggedPlayer.tag.call_deferred()
+	set_hunter(false)
+	score += 1
 	
-	tag.rpc(taggedPlayer.playerId)
-	
+func tag():
 	tagged = true
 	$Camera2D.shake()
 	await get_tree().create_timer(1.0).timeout
 	tagged = false
-	
-	#taggedPlayer.tag.call_deferred()
-	#set_hunter(false)
-	#score += 1
+	set_hunter(true)
 
-@rpc("reliable")
-func tag(taggedPlayerId):
-	score += 1
-	for player in world.player_spawn_target.get_children():
-		if player.playerId == taggedPlayerId:
-			player.hunter = true
-
-	hunter = false
-
+func set_hunter(is_hunter: bool):
+	hunter = is_hunter
+	if hunter:
+		$Control/MarginContainer/NinePatchRect.region_rect = HUNTER_PATCH_RECT
+	else:
+		$Control/MarginContainer/NinePatchRect.region_rect = RUNNER_PATCH_RECT
 	
 @rpc("any_peer", "call_local", "reliable")
 func setName(playerName):
