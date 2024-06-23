ec29b0a84a8732b42ca38c91b80da7d669359dc0
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-06-23 15:32:41 +0200

    More advanced runner bot

--- player/PlayerBotControllStrategy.gd
+++ player/PlayerBotControllStrategy.gd
@@ -1,22 +1,35 @@
 class_name PlayerBotControllStrategy
 extends PlayerControllStrategy
 
-var start_following = false
 var targetPlayer: Player
 var player: Player
 
+const directions = [Vector2i.UP, Vector2i.DOWN, Vector2i.RIGHT, Vector2i.LEFT]
+var _rng = RandomNumberGenerator.new()
+var lastDirection = Vector2i.ZERO
+
 func get_target_vector(position: Vector2i) -> Vector2i:
 	if not player.hunter:
-		return Vector2i.RIGHT
+		var hunterDirection: Vector2i = targetPlayer.targetPos - targetPlayer.fromPos
+		
+		if hunterDirection == Vector2i.ZERO or _rng.randi_range(0, 10) < 1:
+			# When the hunter is not moving (or in ~10% of the moves) go in a random direction.
+			# But prefere the last moving direction.
+			var directionPickList = [lastDirection, lastDirection, lastDirection, lastDirection]
+			directionPickList.append_array(directions)
+			directionPickList.shuffle()
+			
+			lastDirection = directionPickList[0]
+		elif (hunterDirection.x * player.fromPos.x) <= 0 and (hunterDirection.y * player.fromPos.y) <= 0:
+			# Do not follow the hunter if he is running away from "me"
+			lastDirection = hunterDirection * -1
+		else:
+			# Run away from the hunter if he is running in "my" direction
+			lastDirection = hunterDirection
+		
+		return lastDirection
 	else:
 		var delta: Vector2i = (position / GameWorld.GRID_SIZE) - targetPlayer.fromPos
-		
-		# Give the player some head start
-		if not start_following:
-			if delta.length() > 10:
-				start_following = true
-			return Vector2i.ZERO
-		
 		if delta == Vector2i.ZERO:
 			return delta
 		elif (delta.x > delta.y and delta.x != 0) or delta.y == 0:
